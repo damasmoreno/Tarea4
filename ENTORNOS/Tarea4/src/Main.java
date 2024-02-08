e29b43a576fe2c1384ada9ea3b3c12b627c501f6
mdama <mdama@Portatil-Damas>
2024-02-08 15:47:58 +0100

    Modificación 2: Añadir otro código

--- ENTORNOS/Tarea4/src/Main.java
+++ ENTORNOS/Tarea4/src/Main.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 /**
  *
@@ -10,9 +6,7 @@
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
         NuevoMetodo();
 
@@ -69,8 +63,17 @@ public class Main {
                            System.out.print("Fallo…….");
 
                             }
+        try {
+            
+        
+                     System.out.println("Modificación 2");
+                     
+                     cesta.comprar(bonoactual);
+        } catch (Exception e) {
+    
+                        System.out.println("Error");
+        }
     }
-
 }
     
     
